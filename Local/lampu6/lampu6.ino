5e2e2cdf7aeb95fe4bec7ee01bbc7f24af8e4100
richardalvianto <32948848+richardalvianto@users.noreply.github.com>
2017-12-06 10:21:57 +0700

    update local

---
+++ Local/lampu6/lampu6.ino
@@ -0,0 +1,154 @@
+/*
+ Basic ESP8266 MQTT example
+
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "Miskin";
+const char* password = "1234567890";
+const char* mqtt_server = "94.177.202.105";
+int local = -1;
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+int lampu = 15;
+char localMsg[50]; 
+int localLength = 0;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);    // Initialize the BUILTIN_LED pin as an output
+  pinMode(lampu, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH);
+  digitalWrite(lampu,LOW);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 8883);
+  client.setCallback(callback);
+  
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == 'x' && (char)payload[1] == 'x' && (char)payload[7] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    digitalWrite(lampu,HIGH);
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } 
+  else if((char)payload[0] == 'x' && (char)payload[1] == 'x' && (char)payload[7] == '0'){
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+    digitalWrite(lampu,LOW);
+  } else if((char)payload[0] == '0' && (char)payload[1] == '1'){
+    Serial.print("LocalMsg = ");
+     for (int i = 2; i < length; i++) {
+      localMsg[i-2] = payload[i];
+      Serial.print((char)localMsg[i-2]);
+    }
+    localLength = length-2;
+    Serial.print("Panjang localLength = ");
+    Serial.print(localLength);
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("Lampu6","testuser","testpassword")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "Connect LAMPU-6");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      local=(local+1)%localLength;
+      Serial.print(local);
+      if(localMsg[local] == '1'){
+        Serial.println("sementara mati lampu nyala");
+        digitalWrite(lampu,HIGH);
+      }else{
+        Serial.println("sementara mati lampu mati");
+        digitalWrite(lampu,LOW);
+      }
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 75, "LAMPU-6 #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
+}
